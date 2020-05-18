3675f0b65d127d7c1ac80a1d79faa2b1b3ab3e50
Marjolein Niestadt <60259545+Marj0lein@users.noreply.github.com>
2020-05-18 15:32:03 +0200

    Changed image location

--- indexL2.js
+++ indexL2.js
@@ -12,16 +12,16 @@ function delayDicee (){
 
 function playDiceeShow(){
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").src = "dice" + randomNumber1 + ".png";
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-  document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").src = "dice" + randomNumber2 + ".png";
 }
 
 function playDicee(){
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").src = "dice" + randomNumber1 + ".png";
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-  document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").src = "dice" + randomNumber2 + ".png";
   if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
   } else if (randomNumber1 < randomNumber2) {
